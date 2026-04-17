c9d257738e126f0016d8618d083fc182d707ed57
bjarnib23 <bjarnib23@ru.is>
2026-04-17 11:07:56 +0000

    fix: correct CORS Allow-Methods header on availability API to match route definition

--- drupal/web/modules/custom/fkr_booking/src/Controller/AvailabilityController.php
+++ drupal/web/modules/custom/fkr_booking/src/Controller/AvailabilityController.php
@@ -204,7 +204,7 @@ class AvailabilityController extends ControllerBase {
   private function corsHeaders(): array {
     return [
       'Access-Control-Allow-Origin'  => '*',
-      'Access-Control-Allow-Methods' => 'GET, POST, OPTIONS',
+      'Access-Control-Allow-Methods' => 'GET, OPTIONS',
       'Access-Control-Allow-Headers' => 'Content-Type',
     ];
   }
