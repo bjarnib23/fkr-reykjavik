eb3b6e480f87ee3256fd1acea6299af79cd16a0c
bjarnib23 <bjarnib23@ru.is>
2026-04-21 22:47:27 +0000

    feat: add gift card thank-you and cancel pages, wire up React routes

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,27 +1,31 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-  import Navbar from './components/Navbar'
-  import Footer from './components/Footer'
-  import Home from './pages/Home'
-  import Booking from './pages/booking/Booking'  
-  import FAQ from './pages/FAQ'
-  import PriceList from './pages/PriceList'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Booking from './pages/booking/Booking'
+import FAQ from './pages/FAQ'
+import PriceList from './pages/PriceList'
+import GiftCard from './pages/GiftCard'
+import GiftCardThankYou from './pages/GiftCardThankYou'
+import GiftCardCancel from './pages/GiftCardCancel'
 
-                                                                    
-  function App() {
-    return (                                                        
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/boka-tima" element={<Booking />} />
-          <Route path="/ferlid" element={<p>Ferlið</p>} />
-          <Route path="/algengar-spurningar" element={<FAQ />} />
-          <Route path="/gjafabref" element={<p>Gjafabréf</p>} />
-          <Route path="/verdskra" element={<PriceList />} />
-        </Routes>
-        <Footer/>
-      </BrowserRouter>
-    )
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/boka-tima" element={<Booking />} />
+        <Route path="/ferlid" element={<p>Ferlið</p>} />
+        <Route path="/algengar-spurningar" element={<FAQ />} />
+        <Route path="/gjafabref" element={<GiftCard />} />
+        <Route path="/verdskra" element={<PriceList />} />
+        <Route path="/giftcard/thank-you" element={<GiftCardThankYou />} />
+        <Route path="/giftcard/cancel" element={<GiftCardCancel />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
+}
 
-  export default App
+export default App
